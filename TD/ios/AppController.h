feb3f5e54f8d2d2d224f59aee2e89880649dcc20
Yifeng Wu <yifengwu@Artist-Woods.local>
2012-12-19 14:34:03 +0800

    Initial Commit

---
+++ TD/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  TDAppController.h
+//  TD
+//
+//  Created by Yifeng Wu on 19/12/12.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ TD/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  TDAppController.h
+//  TD
+//
+//  Created by Yifeng Wu on 19/12/12.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
